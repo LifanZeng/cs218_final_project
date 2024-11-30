8431b084c412b6da92d18691d4338894fcee3791
LifanZeng <73094918+LifanZeng@users.noreply.github.com>
2024-11-30 04:44:56 -0800

    Update application.py

--- application.py
+++ application.py
@@ -27,4 +27,4 @@ def seq_with_beginning():
     # return render_template('index_with_output.html', my_output = sequences[0])
 
 if __name__ == '__main__':
-    application.run(host= "0.0.0.0", port=5000, debug=True)
+    application.run(host= "0.0.0.0", port=5000)
